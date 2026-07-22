c928f31e26eca9628f14aa6d1a11d2ff8ea084da
Matthew Friedman <mafr6311@colorado.edu>
2026-07-22 15:23:58 -0700

    implemented linux build script; needed to update finder scripts to find conf instead of ../conf and finder.sh to use sh instead of bash

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -32,7 +32,8 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+#assignment=`cat ../conf/assignment.txt`
+assignment=`cat conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -49,15 +50,13 @@ then
 	fi
 fi
 
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
-
 for i in $( seq 1 $NUMFILES)
 do
 	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
+ls
+ls -l finder.sh
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -13,6 +13,8 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
+WRITER_APP_DIR="/home/matt-coursera/Documents/course1/assignment3/repo/assignment-3-part-1-Mfrie008/finder-app"
+
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -35,7 +37,24 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    
+    # deep clean
+    # debug commented out for speed
+    #make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
+    
+    # make virtual target .config
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+    
+    # make image
+    make -j8 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+    
+    # make modules
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+    
+    # make device tree
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
 fi
+
 
 echo "Adding the Image in outdir"
 
@@ -48,6 +67,13 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys temp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+cd ../
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +82,62 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    # Not sure if there's anything to do here?
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+# debug commented out for speed
+#make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE distclean
+make -j8 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+make -j8 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE install
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+#echo "Library dependencies"
+#${CROSS_COMPILE}readelf -a /tmp/aeld/busybox | grep "program interpreter"
+#${CROSS_COMPILE}readelf -a /tmp/aeld/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+# Need to find where all the library dependencies live - credit to github user Cneema1757
+ARCH64_CC_SYSROOT="$(aarch64-none-linux-gnu-gcc -print-sysroot)"
+
+cp $ARCH64_CC_SYSROOT/lib/ld-linux-aarch64.so.1 "${OUTDIR}/rootfs/lib"
+cp $ARCH64_CC_SYSROOT/lib64/libm.so.6 "${OUTDIR}/rootfs/lib64"
+cp $ARCH64_CC_SYSROOT/lib64/libresolv.so.2 "${OUTDIR}/rootfs/lib64"
+cp $ARCH64_CC_SYSROOT/lib64/libc.so.6 "${OUTDIR}/rootfs/lib64"
+
 
 # TODO: Make device nodes
+sudo mknod -m 666 "${OUTDIR}/rootfs/dev/null" c 1 3
+sudo mknod -m 622 "${OUTDIR}/rootfs/dev/console" c 5 1
+
 
 # TODO: Clean and build the writer utility
+cd $WRITER_APP_DIR
+make clean
+make CROSS_COMPILE=$CROSS_COMPILE
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp "$WRITER_APP_DIR/writer" "${OUTDIR}/rootfs/home"
+cp "$WRITER_APP_DIR/finder.sh" "${OUTDIR}/rootfs/home"
+cp "$WRITER_APP_DIR/finder-test.sh" "${OUTDIR}/rootfs/home"
+cp "$WRITER_APP_DIR/autorun-qemu.sh" "${OUTDIR}/rootfs/home"
+
+# need to also include username.txt in /conf for writer/finder to work
+mkdir ${OUTDIR}/rootfs/home/conf
+cp conf/username.txt ${OUTDIR}/rootfs/home/conf
+cp conf/assignment.txt ${OUTDIR}/rootfs/home/conf
 
 # TODO: Chown the root directory
+sudo chown -R root:root "${OUTDIR}/rootfs"
 
 # TODO: Create initramfs.cpio.gz
+cd "${OUTDIR}/rootfs"
+find . | cpio -H newc -ov --owner root:root > $OUTDIR/initramfs.cpio
+cd "${OUTDIR}"
+gzip -f $OUTDIR/initramfs.cpio
+
+# Copy compiled linux image to outdir
+sudo cp /tmp/aeld/linux-stable/arch/arm64/boot/Image $OUTDIR
