8dfa336a2042f28d27907ca5fa3e0f8ab18cec86
Matthew Friedman <mafr6311@colorado.edu>
2026-07-26 17:57:33 -0700

    fixed bugs regarding func arg types

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,7 +14,7 @@ static long unsigned int getMsFromClock(void)
 	struct timespec thisTime;
 	clock_gettime(CLOCK_MONOTONIC, &thisTime);
 	
-	nowTime = (thisTime->tv_sec * 1000) + (thisTime->tv_nsec / 1000000);
+	nowTime = (thisTime.tv_sec * 1000) + (thisTime.tv_nsec / 1000000);
 	return nowTime;
 }
 
@@ -27,13 +27,13 @@ void* threadfunc(void* thread_param)
 	// hint: use a cast like the one below to obtain thread arguments from your parameter
 	//struct thread_data* thread_func_args = (struct thread_data *) thread_param;
 	
-	now = getMsFromClock();
-	while(getMsFromClock() < (now + thisData->waitGet));
+	start = getMsFromClock();
+	while((now = getMsFromClock()) < (start + thisData->waitGet));
 	
 	int lockStat = pthread_mutex_lock(thisData->inputMutex);
 	
-	now = getMsFromClock();
-	while(getMsFromClock() < (now + thisData->waitRelease));
+	start = getMsFromClock();
+	while((now = getMsFromClock()) < (start + thisData->waitRelease));
 	
 	int relStat = pthread_mutex_unlock(thisData->inputMutex);
 	
@@ -54,9 +54,9 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
 	 * See implementation details in threading.h file comment block
 	 */
 
-	pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_init(mutex, NULL);
 
-	struct thread_data* myData = malloc(sizeof(thread_data));
+	struct thread_data* myData = malloc(sizeof(struct thread_data));
 
 	myData->inputMutex = mutex;
 	myData->waitGet= wait_to_obtain_ms;
