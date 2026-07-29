832bf85cfd179df4e72c4a29837b0e7e3b8ca922
Matthew Friedman <mafr6311@colorado.edu>
2026-07-29 10:05:19 -0700

    fixed error with result file path

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -59,7 +59,7 @@ done
 #ls -l finder.sh
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
-echo $OUTPUTSTRING > assignment4-result.txt
+echo $OUTPUTSTRING > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
